ce0f89738f030e63dcfd1a4a77334b27d49e393c
Jasterix <ljcalixte@gmail.com>
2019-12-15 00:36:31 -0500

    feature countdown from 60

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 const title = document.getElementById("title");
 
-console.log(title);
+const timer = () => {
+  let interval = 60;
+  const timer = setInterval(() => {
+    title.innerHTML = "00:" + interval;
+    console.log(interval);
+    interval--;
+    if ((interval, 0)) {
+      clearInterval(timer);
+    }
+  }, 1000);
+};
+timer();
